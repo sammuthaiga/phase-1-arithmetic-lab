c140ad10e27b8db8d836aa876f618f0ddc96114e
Samuel <samuel.muthaiga@student.moringaschool.com>
2022-11-22 02:27:52 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,2 +1,9 @@
-
 // Write your code here
+const num1 = 2;
+const num2 = 31;
+const multiply = num1 * num2;
+const random = Math.floor(Math.random() * 0) + 1;
+const num3 = 25;
+const num4 = 7;
+const mod = num3 % num4;
+const max = Math.max(15, 16, 17, 18, 19, 20);
